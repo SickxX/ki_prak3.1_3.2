cb4227a6b6aae59cc95780632eea8d1bc53fada7
MaHP <MaHP@MarvinHP>
2019-01-08 12:31:11 +0100

    removed unused imports

--- RoboCentral/src/huston/MapBuilder.java
+++ RoboCentral/src/huston/MapBuilder.java
@@ -1,16 +1,10 @@
 package huston;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.File;
-import java.io.IOException;
-
 import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
