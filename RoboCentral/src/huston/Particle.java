725f5a295254f7aa4ba7e81ce5319c4e9c71f03c
MaHP <MaHP@MarvinHP>
2019-01-16 11:46:22 +0100

    Particles are able to move

--- RoboCentral/src/huston/Particle.java
+++ RoboCentral/src/huston/Particle.java
@@ -5,58 +5,74 @@ import java.awt.Graphics;
 
 public class Particle {
 
-	protected int id,x,y;
-	protected int[] dir;
+	protected double id,x,y;
+	protected double[] dir;
+	protected float angle;
 	
-	public Particle(int id, int x, int y) {
+	public Particle(double id, double x, double y) {
 		this.id = id;
 		this.x = x;
 		this.y = y;
 	}
-	public Particle(int x, int y) {
+	public Particle(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 	
-	public Particle(int id, int x, int y, int[] dir) {
+	public Particle(double id, double x, double y, double[] dir) {
 		this.id = id;
 		this.x = x;
 		this.y = y;
 		this.dir= dir;
 	}
-	public int getId() {
+	public Particle(double id, double x, double y, float angle) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.angle = angle;
+	}
+	public float getAngle() {
+		return angle;
+	}
+	public double getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(double id) {
 		this.id = id;
 	}
-	public int getX() {
+	public double getX() {
 		return x;
 	}
-	public void setX(int x) {
+	public void setX(double x) {
 		this.x = x;
 	}
-	public int getY() {
+	public double getY() {
 		return y;
 	}
-	public void setY(int y) {
+	public void setY(double y) {
 		this.y = y;
 	}
-	public int[] getDir() {
+	public double[] getDir() {
 		return dir;
 	}
-	public void setDir(int[] dir) {
+	public void setDir(double[] dir) {
 		this.dir = dir;
 	}
 	
 	public void draw(Graphics g, float scale)
 	{
 //		g.setColor(c);
-		g.fillOval(getX(), getY(), 3, 3);
+		g.fillOval((int)getX(),(int) getY(), 3, 3);
 //		g.drawLine(from.getX() + Vertex.SIZE/2, from.getY() + Vertex.SIZE/2, to.getX() + Vertex.SIZE/2, to.getY() + Vertex.SIZE/2);
 		g.setColor(Color.RED);
 		//g.drawLine((int) ((from.getX() + Vertex.SIZE/2) * scale), from.getY() + (int) ((from.getX() + Vertex.SIZE/2 * scale)), (int) ((to.getX() + Vertex.SIZE/2) * scale), (int) ((to.getY() + Vertex.SIZE/2) * scale));
 	}
-	
-	
+	/**
+	 * 
+	 * @param distance distance in centimeters, distance that the particle moves
+	 */
+	public void moveParticle(int distance) {
+		setY(y + Math.sin(getAngle())*distance);
+		setX(x + Math.cos(getAngle())*distance);
+	}	
 }
