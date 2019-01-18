e0dccf9cbfc390c54d43de76deefe42b1af4da99
MaHP <MaHP@MarvinHP>
2019-01-18 13:10:56 +0100

    NextStep

--- RoboCentral/src/huston/RobotServer.java
+++ RoboCentral/src/huston/RobotServer.java
@@ -14,50 +14,51 @@ public class RobotServer
 
 	private static RobotServer server;
 	private MapContainer mc;
-	
+
 	public static void main(String[] args) throws IOException 
 	{
 		// instance of map and graphics and stuff
 		server = new RobotServer();
 	}
-	
+
 	public RobotServer()
 	{
 		mc = new MapContainer();
 		mc.getMCA();
-		
+
 
 		//Conection
 		try {
-		connect();
-	}catch(IOException e){
-		e.printStackTrace();
+			connect();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
-	}
- 
+
 	public void connect() throws IOException 
 	{
 		System.out.println("Starting");
 		int port = 8083;
-		
+
 		java.net.ServerSocket serverSocket = new java.net.ServerSocket(port);
+		System.out.println("Waiting...");
 		java.net.Socket client = waitForLogin(serverSocket);
 		System.out.println("Connected");
 		try{
-//			call(client, "Sensors");
-//			call(client, "Forward 500");
-//			call(client, "Sensors");
-//			call(client, "TurnLeft 90");
-//			call(client, "TurnRight 270");
-//			call(client, "Look 90");
-//			call(client, "Look -180");
-//			call(client, "Sensors");
-//			call(client, "Forward 500");
-//			call(client, "Sensors");
-//			call(client, "TurnLeft 180");
-//			call(client, "Sensors");
-//			call(client, "Kill");
-			
+			//			call(client, "Sensors");
+			//			call(client, "Forward 500");
+			//			call(client, "Sensors");
+			//			call(client, "TurnLeft 90");
+			//			call(client, "TurnRight 270");
+			//			call(client, "Look 90");
+			//			call(client, "Look -180");
+			//			call(client, "Sensors");
+			//			call(client, "Forward 500");
+			//			call(client, "Sensors");
+			//			call(client, "TurnLeft 180");
+			//			call(client, "Sensors");
+			//			call(client, "Kill");
+
 			//1. Partikel erzeugen
 			mc.getMCA().start();
 			delay(1000);
@@ -80,16 +81,16 @@ public class RobotServer
 			}
 			//3. 
 			//3. 
-			
+
 		} catch(Exception e)	{
 			e.printStackTrace();
 			write(client, "Kill");
 			client.close();
 			serverSocket.close();
 		}
-			
+
 	}
-	
+
 	public String call(java.net.Socket socket, String command) throws Exception
 	{
 		write(socket, command);
@@ -115,7 +116,7 @@ public class RobotServer
 		java.net.Socket socket = serverSocket.accept();
 		return socket;
 	}
-	
+
 	public void write(java.net.Socket socket, String msg) throws IOException
 	{
 		PrintWriter printwriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
@@ -131,11 +132,11 @@ public class RobotServer
 		String msg = new String(buffer, 0, bSize);
 		return msg;
 	}
-	
+
 	public void measure(java.net.Socket client, int samplesize) throws Exception
 	{
 		ArrayList<SensorData> data = new ArrayList<>();
-		
+
 		double step = 0.0;
 		if(samplesize % 2 == 1) {
 			step = (double)(samplesize - 1) / 2;
@@ -144,7 +145,7 @@ public class RobotServer
 		} else {
 			step = (double)(samplesize / 2);
 		}
-		
+
 		int i = -90;
 		int j = 90;
 		int run = (int) step;
@@ -157,24 +158,25 @@ public class RobotServer
 			j -= 90 / run;
 			run -= 1;
 		}
-		
+
 		mc.getMCA().recalculateParticles(data);
+		nextStep(client, data);
 	}
-	
+
 	public void move(java.net.Socket client, int distance) throws Exception
 	{
 		call(client, "Forward " + distance);
 		mc.getMCA().moveParticles(distance);
 		measure(client, 3);
 	}
-	
+
 	public void turn(java.net.Socket client, int theta) throws Exception
 	{
 		call(client, "Turn " + theta);
 		mc.getMCA().turnParticles(theta);
 		measure(client, 3);
 	}
-	
+
 	public void delay(long delay)
 	{
 		try {
@@ -182,5 +184,30 @@ public class RobotServer
 		}
 		catch(Exception e)
 		{}
+	}
+	public void nextStep(java.net.Socket client ,ArrayList<SensorData> data) {
+		SensorData bestValue = new SensorData(0,0);
+		for(SensorData d: data) {
+			if(d.getDistance() > bestValue.getDistance()) {
+				bestValue = d;
+			}
+			if(d.getDistance() == Float.POSITIVE_INFINITY) {
+				bestValue = d;
+			}
+		}
+		if ( bestValue.getDistance() < 15) {
+			try {
+				turn(client, 180);
+				measure(client, 3);
+			} catch (Exception e) {
+			}
+		}else{
+			try {
+				turn(client,(int)bestValue.getRotation());
+				move(client, 15);
+				measure(client,3);
+			} catch (Exception e) {
+			}
+		}
 	}
 }
